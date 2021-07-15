3d046dc1f095b9b3011878774866b2530102f6f0
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-07-15 12:24:52 -0500

    finished button working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,11 +5,31 @@ const Todo = ({ text, todos, setTodos, todo }) => {
     const deleteHandler = () => {
         setTodos(todos.filter((el) => el.id !== todo.id))
     }
+    // const finishedHandler = () => {
+    //   setTodos(todos.map((item) => {
+    //       if(item.id === todo.id) {
+    //           return {
+    //               ...item, finished: !item.finished
+    //           }
+    //       }
+    //       return item;
+    //   }))
+    // }
 
+    const completeHandler = () => {
+        setTodos(todos.map(item => {
+            if(item.id === todo.id){
+                return{
+                    ...item, finished: !item.finished
+                }
+            }
+            return item;
+        }))
+    }
     return (
         <div className="todo">
-            <li className="todo-item">{text}</li>
-            <button className="todo-btn">Finished</button>
+            <li className={`todo-item ${todo.finished ? "finished" : ""}`}>{text}</li>
+            <button onClick={completeHandler} className="todo-btn">Finished</button>
             <button onClick={deleteHandler} className="todo-btn-2">Delete</button>
         </div>
     )
